dd41507cbdb9ce8d57b19ef53af251f0b358d52b
hsidd1 <hamzasiddiqui247@gmail.com>
2023-10-04 16:54:35 -0400

    login page and registration setup

--- DCM_group/app.py
+++ DCM_group/app.py
@@ -43,96 +43,108 @@ class Database:
 
 Database = Database()
 
-root = tk.Tk()
-root.title("DCM Application")
-root.geometry("600x500")
-# root.configure(bg="#8a8d91") 3 a nice grey colour
-# root.attributes('-fullscreen', True)
+def welcome_screen():
+    welcome_page = tk.Tk()
+    welcome_page.title("DCM Application - Welcome Page")
+    welcome_page.geometry("600x500")
+    # welcome_page.configure(bg="#8a8d91") 3 a nice grey colour
+    # welcome_page.attributes('-fullscreen', True)
 
-tk.Label(root, text="Pacemaker Device Controller-Monitor", font=("Helvetica", 25, "bold")).pack()
-tk.Label(root, text="").pack()
+    tk.Label(welcome_page, text="Pacemaker Device Controller-Monitor", font=("Helvetica", 25, "bold")).pack()
+    tk.Label(welcome_page, text="").pack()
 
-# username labels
-username_label = tk.Label(root, text="Username:")
-username_label.pack()
+    # username labels
+    username_label = tk.Label(welcome_page, text="Username:")
+    username_label.pack()
 
-# username entry
-username_entry = tk.Entry(root, text="username")
-username_entry.pack()
+    # username entry
+    username_entry = tk.Entry(welcome_page, text="username")
+    username_entry.pack()
 
-# password labels
-password_label = tk.Label(root, text="Password:")
-password_label.pack()
+    # password labels
+    password_label = tk.Label(welcome_page, text="Password:")
+    password_label.pack()
 
-# password entry
-password_entry = tk.Entry(root, text="password", show='*')
-password_entry.pack()
+    # password entry
+    password_entry = tk.Entry(welcome_page, text="password", show='*')
+    password_entry.pack()
 
-# function for register button
-def register_user():
-    username_info = username_entry.get()
-    password_info = password_entry.get()
+    # function for register button
+    def register_user():
+        username_info = username_entry.get()
+        password_info = password_entry.get()
 
-    # Error handling
-    if not username_info or not password_info:
-        # Empty string
-        message = tk.Label(root, text="Please enter valid username and password", fg="red", font=("calibri", 11))
+        # Error handling
+        if not username_info or not password_info:
+            # Empty string
+            message = tk.Label(welcome_page, text="Please enter valid username and password", fg="red", font=("calibri", 11))
+            message.pack()
+            welcome_page.after(2000, lambda: message.destroy())
+            return
+        if username_info in Database.users_map.keys():
+            # Username already exists
+            message = tk.Label(welcome_page, text="Username already exists", fg="red", font=("calibri", 11))
+            message.pack()
+            welcome_page.after(2000, lambda: message.destroy())
+            return
+        if Database.get_user_count() >= 10:
+            # Too many users
+            message = tk.Label(welcome_page, text="Too many users", fg="red", font=("calibri", 11))
+            message.pack()
+            welcome_page.after(2000, lambda: message.destroy())
+            return
+        
+        registered_user = User(username_info, password_info)
+        Database.write_to_file(registered_user)
+
+        username_entry.delete(0, tk.END)
+        password_entry.delete(0, tk.END)
+
+        message = tk.Label(welcome_page, text="Registration Success", fg="green", font=("calibri", 11))
         message.pack()
-        root.after(2000, lambda: message.destroy())
-        return
-    if username_info in Database.users_map.keys():
-        # Username already exists
-        message = tk.Label(root, text="Username already exists", fg="red", font=("calibri", 11))
+        welcome_page.after(2000, lambda: message.destroy())
+
+    def login_user():
+        username_info = username_entry.get()
+        password_info = password_entry.get()
+
+        if not username_info or not password_info:
+            # Empty string
+            message = tk.Label(welcome_page, text="Please enter valid username and password", fg="red", font=("calibri", 11))
+            message.pack()
+            welcome_page.after(2000, lambda: message.destroy())
+            return
+        if username_info not in Database.users_map.keys():
+            # Username does not exist
+            message = tk.Label(welcome_page, text="User does not exist", fg="red", font=("calibri", 11))
+            message.pack()
+            welcome_page.after(2000, lambda: message.destroy())
+            return
+        if Database.users_map[username_info] != password_info:
+            # Incorrect password
+            message = tk.Label(welcome_page, text="Incorrect password", fg="red", font=("calibri", 11))
+            message.pack()
+            welcome_page.after(2000, lambda: message.destroy())
+            return
+        
+        # Login success
+        message = tk.Label(welcome_page, text="Login Success", fg="green", font=("calibri", 11))
         message.pack()
-        root.after(2000, lambda: message.destroy())
-        return
-    if Database.get_user_count() >= 10:
-        # Too many users
-        message = tk.Label(root, text="Too many users", fg="red", font=("calibri", 11))
-        message.pack()
-        root.after(2000, lambda: message.destroy())
-        return
-    
-    registered_user = User(username_info, password_info)
-    Database.write_to_file(registered_user)
+        # open home page 
+        homepage_screen()
+        welcome_page.destroy()
 
-    username_entry.delete(0, tk.END)
-    password_entry.delete(0, tk.END)
+    login_btn    = tk.Button(welcome_page, text="Login", width=10, height=1, bg="#eda758", command = login_user).pack(pady=10)
+    register_btn = tk.Button(welcome_page, text="Register", width=10, height=1, bg="#eda758", command = register_user).pack()
 
-    message = tk.Label(root, text="Registration Success", fg="green", font=("calibri", 11))
-    message.pack()
-    root.after(2000, lambda: message.destroy())
+    welcome_page.mainloop()
 
-def login_user():
-    username_info = username_entry.get()
-    password_info = password_entry.get()
+def homepage_screen():
+    homepage_screen = tk.Tk()
+    homepage_screen.title("DCM Application - Home Page")
+    homepage_screen.geometry("600x500")
+    # homepage_screen.configure(bg="#8a8d91") 3 a nice grey colour
 
-    if not username_info or not password_info:
-        # Empty string
-        message = tk.Label(root, text="Please enter valid username and password", fg="red", font=("calibri", 11))
-        message.pack()
-        root.after(2000, lambda: message.destroy())
-        return
-    if username_info not in Database.users_map.keys():
-        # Username does not exist
-        message = tk.Label(root, text="Username does not exist", fg="red", font=("calibri", 11))
-        message.pack()
-        root.after(2000, lambda: message.destroy())
-        return
-    if Database.users_map[username_info] != password_info:
-        # Incorrect password
-        message = tk.Label(root, text="Incorrect password", fg="red", font=("calibri", 11))
-        message.pack()
-        root.after(2000, lambda: message.destroy())
-        return
-    
-    # Login success
-    message = tk.Label(root, text="Login Success", fg="green", font=("calibri", 11))
-    message.pack()
-    # open home page 
-
-
-login_btn    = tk.Button(root, text="Login", width=10, height=1, bg="#eda758", command = login_user).pack(pady=10)
-register_btn = tk.Button(root, text="Register", width=10, height=1, bg="#eda758", command = register_user).pack()
-
-root.mainloop()
+if __name__ == "__main__":
+    # start the application
+    welcome_screen()
