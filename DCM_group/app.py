f71f063ee0463453e4ba4891a153b806442879cb
hsidd1 <hamzasiddiqui247@gmail.com>
2023-10-06 10:57:35 -0400

    docstrings, private methods, and fix issue where image size was covering login/registration error messages

--- DCM_group/app.py
+++ DCM_group/app.py
@@ -1,10 +1,11 @@
 import tkinter as tk 
 from PIL import ImageTk, Image
-# import os 
 
 DATABASE = "DCM_group/database.txt"
 
 class User:
+    """User class to store username and password
+       possibly extend to store pacemaker data"""
     def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
@@ -18,6 +19,7 @@ class User:
         return f"User with username: {self.username} and password: {self.password}"
 
 class Database:
+    """Database class to store Users"""
     def __init__(self, database: str = DATABASE):
         self.database = database
         # map of users' usernames and passwords
@@ -26,25 +28,28 @@ class Database:
             self.users_map = {line.split()[0]: line.split()[1] for line in f.readlines() if line}
 
     def get_user_count(self) -> int:
+        """returns number of users in database"""
         return len(self.users_map)
 
-    def add_user(self, user: User):
+    def __add_user(self, user: User):
+        """Private method - adds user to database"""
         self.users_map[user.username] = user.password
 
-    def remove_user(self, username: str):
+    def __remove_user(self, username: str):
+        """Private method - removes user from database"""
         del self.users_map[username]
 
     def write_to_file(self, user: User):
-        self.add_user(user)
-        if user in self.users_map:
-            user.exists = True # unused for now
-        else:
+        """writes user to database when registered"""
+        self.__add_user(user)
+        if not user in self.users_map:
             with open(self.database, "a") as f:
                 f.write(f"{user.user_string()}\n")
 
 Database = Database()
 
 def welcome_screen():
+    """Welcome page for login and registration of users"""
     welcome_page = tk.Tk()
     welcome_page.title("DCM Application - Welcome Page")
     welcome_page.geometry("600x500")
@@ -69,31 +74,32 @@ def welcome_screen():
     password_entry = tk.Entry(welcome_page, text="password", show='*')
     password_entry.pack()
 
-    # function for register button
     def register_user():
+        """Registers user and writes to file if valid"""
+
         username_info = username_entry.get()
         password_info = password_entry.get()
 
-        # Error handling
+        if Database.get_user_count() >= 10:
+            # Too many users
+            message = tk.Label(welcome_page, text="Too many users", fg="red", background="#8a8d91", font=("calibri", 11))
+            message.pack()
+            welcome_page.after(2000, lambda: message.destroy())
+            return
         if not username_info or not password_info:
-            # Empty string
-            message = tk.Label(welcome_page, text="Please enter valid username and password", fg="red", font=("calibri", 11))
+            # Empty entry
+            message = tk.Label(welcome_page, text="Please enter valid username and password", fg="red", background="#8a8d91", font=("calibri", 11))
             message.pack()
             welcome_page.after(2000, lambda: message.destroy())
             return
         if username_info in Database.users_map.keys():
             # Username already exists
-            message = tk.Label(welcome_page, text="Username already exists", fg="red", font=("calibri", 11))
-            message.pack()
-            welcome_page.after(2000, lambda: message.destroy())
-            return
-        if Database.get_user_count() >= 10:
-            # Too many users
-            message = tk.Label(welcome_page, text="Too many users", fg="red", font=("calibri", 11))
+            message = tk.Label(welcome_page, text="Username already exists", fg="red", background="#8a8d91", font=("calibri", 11))
             message.pack()
             welcome_page.after(2000, lambda: message.destroy())
             return
         
+        # create registered user object
         registered_user = User(username_info, password_info)
         Database.write_to_file(registered_user)
 
@@ -105,6 +111,7 @@ def welcome_screen():
         welcome_page.after(2000, lambda: message.destroy())
 
     def login_user():
+        """Logs user into homepage if user exists and if password is correct"""
         username_info = username_entry.get()
         password_info = password_entry.get()
 
@@ -133,12 +140,16 @@ def welcome_screen():
         # open home page 
         homepage_screen()
         welcome_page.destroy()
-    # Buttons
-    login_btn    = tk.Button(welcome_page, text="Login", width=10, height=1, bg="#eda758", command = login_user).pack(pady=10)
-    register_btn = tk.Button(welcome_page, text="Register", width=10, height=1, bg="#eda758", command = register_user).pack()
+
+    # Log in and register buttons
+    login_btn    = tk.Button(welcome_page, text="Login", width=10, height=1, bg="#eda758", command = login_user)
+    login_btn.pack(pady=10)
+    register_btn = tk.Button(welcome_page, text="Register", width=10, height=1, bg="#eda758", command = register_user)
+    register_btn.pack()
+
     # Image logo
     logo = Image.open("DCM_group/imgs/heartLogo.png")
-    logo_resized = logo.resize((200, 200))
+    logo_resized = logo.resize((100, 100)) # maximum size to be able to still display three error messages on screen
     logo_resized = ImageTk.PhotoImage(logo_resized)
     logo_label = tk.Label(welcome_page, image = logo_resized, background="#8a8d91")
     logo_label.pack(side="bottom", pady=50)
@@ -146,6 +157,8 @@ def welcome_screen():
     welcome_page.mainloop()
 
 def homepage_screen():
+    """Homepage screen for choosing pacing mode and viewing pacing data
+       Takes User to pacing_display_screen() when all parameters are filled in"""
     # configuring the page attributes
     homepage_screen = tk.Tk()
     homepage_screen.title("DCM Application - Home Page")
