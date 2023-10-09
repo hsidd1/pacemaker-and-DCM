05fba764964ed4725bb8eb79bf3953c8f6bb08b1
Amr Elhossan <thehorse2003@gmail.com>
2023-10-09 02:10:24 -0400

    parameters save into database, database format has also been changed

--- DCM_group9/app.py
+++ DCM_group9/app.py
@@ -3,6 +3,8 @@ from PIL import ImageTk, Image
 from database import Database
 
 Database = Database()
+current_user = None
+
 
 def welcome_screen():
     """Welcome page for login and registration of users"""
@@ -37,8 +39,10 @@ def welcome_screen():
     def login_user():
         """wrapper for Database.login_user()
            takes user to homepage_screen() if successful"""
+        global current_user 
+        current_user = username_entry.get()
         Database.login_user(welcome_page=welcome_page, username_entry=username_entry, password_entry=password_entry, homepage_screen=homepage_screen)
-
+ 
     # Log in and register buttons
     login_btn    = tk.Button(welcome_page, text="Login", width=10, height=1, bg="#eda758", command = login_user)
     login_btn.pack(pady=10)
@@ -127,7 +131,22 @@ def settings_screen(pacing_mode: str):
     
     def apply():
         #TODO: update all parameters in database
-        pass
+        param_data = {
+            "l_rate_limit": LowerRate_entry.get(),
+            "u_rate_limit": UpperRate_entry.get(),
+            "atrial_amplitude": AtrialAmp_entry.get(),
+            "atrial_pulse_width": AtrialPulseWidth_entry.get(),
+            "ARP": Arp_entry.get(),
+            "vent_amplitude": VentricularAmp_entry.get(),
+            "vent_pulse_width": VentricularPulseWidth_entry.get(),
+            "VRP": Vrp_entry.get()             
+        }
+
+        temp_data = dict(param_data)
+        for key in temp_data:
+            if (not len(temp_data[key])):
+                del param_data[key]
+        Database.update_parameters(current_user, pacing_mode, param_data)
 
     def ok():
         # apply and close
--- DCM_group9/user.py
+++ DCM_group9/user.py
@@ -1,9 +1,38 @@
 class User:
     """User class to store username and password
        possibly extend to store pacemaker data"""
+
     def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
+        self.__parameter_dict = {
+            "AOO": {
+                "l_rate_limit": "No Data",
+                "u_rate_limit": "No Data",
+                "atrial_amplitude": "No Data",
+                "atrial_pulse_width": "No Data"
+            }, 
+            "AAI": {
+                "l_rate_limit": "No Data",
+                "u_rate_limit": "No Data",
+                "atrial_amplitude": "No Data",
+                "atrial_pulse_width": "No Data",
+                "ARP": "No Data"
+            },
+            "VOO": {
+                "l_rate_limit": "No Data",
+                "u_rate_limit": "No Data",
+                "vent_amplitude": "No Data",
+                "vent_pulse_width": "No Data"
+            },
+            "VVI": {
+                "l_rate_limit": "No Data",
+                "u_rate_limit": "No Data",
+                "vent_amplitude": "No Data",
+                "vent_pulse_width": "No Data",
+                "VRP": "No Data"
+            }
+        }
 
     def user_string(self) -> str:
         """for *text* file writing"""
@@ -15,4 +44,7 @@ class User:
     
     def to_dict(self) -> dict:
         """for *json* file writing"""
-        return {"username": self.username, "password": self.password}
+        return {self.username: {"password": self.password, "pacing_mode_params": self.__parameter_dict}}
+    
+    def update_parameters(self, pacing_mode: str, data: dict):
+        self.__parameter_dict[pacing_mode] = data
