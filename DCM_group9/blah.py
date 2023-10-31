d8ca84f86a2f67aef53958d74667e367183b3c51
hsidd1 <hamzasiddiqui247@gmail.com>
2023-10-31 04:35:21 -0400

    formatting test

--- DCM_group9/blah.py
+++ DCM_group9/blah.py
@@ -1,67 +1,11 @@
-# poorly formatted code to test black formatter on
+# poorly formatted code to test black formatter on 
 
-
-def goofy_function(
-    a,
-    b,
-    v,
-    c,
-    d,
-    e,
-    f,
-    g,
-    h,
-    j,
-    sjdfksjdflk,
-    sfsdg,
-    fg,
-    gfgsg,
-    fgfgdfgfgfsg,
-    gegerhtehtfdb,
-    fdfbdfb,
-):
+def goofy_function(a,b,v,c,d,e,f,g,h,j,sjdfksjdflk,sfsdg,fg,gfgsg,fgfgdfgfgfsg,gegerhtehtfdb,fdfbdfb):
     "fhjdshgorehgoerhgjfdhgjldfhgldfjhgdfhj ihjgrshg hg hjfiogjh i ji j ij gijgijh h jghofghigjetitjhiteji jijhgifoj hi j ifgjigjdgij"
-    return (
-        a
-        + b
-        + c
-        + d
-        + e
-        + f
-        + g
-        + h
-        + j
-        + sjdfksjdflk
-        + sfsdg
-        + fg
-        + gfgsg
-        + fgfgdfgfgfsg
-        + gegerhtehtfdb
-        + fdfbdfb
-    )
+    return a+b+c+d+e+f+g+h+j+sjdfksjdflk+sfsdg+fg+gfgsg+fgfgdfgfgfsg+gegerhtehtfdb+fdfbdfb
 
-
-def goofy_function2(
-    a,
-    b,
-    v,
-    c,
-    d,
-    e,
-    f,
-    g,
-    h,
-    j,
-    sjdfksjdflk,
-    sfsdg,
-    fg,
-    gfgsg,
-    fgfgdfgfgfsg,
-    gegerhtehtfdb,
-    fdfbdfb,
-):
-    if a == b or v == c or d == g and j == e:
+def goofy_function2(a,b,v,c,d,e,f,g,h,j,sjdfksjdflk,sfsdg,fg,gfgsg,fgfgdfgfgfsg,gegerhtehtfdb,fdfbdfb):
+    if (a == b or v == c or d == g and j == e):
         return True
-
-
-print("hi")
+    
+print ('hi')
